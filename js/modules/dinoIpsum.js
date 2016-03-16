8247eb3acccf1dd1be892e545e670e02495ad6f8
Joseph Karasek and Nicholas Jensen-Hay <nicholasjensenhay@gmail.com>
2016-03-16 14:44:47 -0700

    humidity also gets a dino

--- js/modules/dinoIpsum.js
+++ js/modules/dinoIpsum.js
@@ -1,6 +1,11 @@
 exports.warning = function() {
   $.get('http://dinoipsum.herokuapp.com/api/?format=text&words=1&paragraphs=1').done(function(response) {
-    console.log(response);
     $('#city_name').append(" and watch out for <strong>" + response + "</strong>");
   });
 };
+
+exports.idealConditions = function() {
+  $.get('http://dinoipsum.herokuapp.com/api/?format=text&words=1&paragraphs=1').done(function(response) {
+    $('#humidity').append(", which is the right humidity for <strong>" + response + "</strong>");
+  });
+}
